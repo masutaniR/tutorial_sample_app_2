5401362fc4cbf97ddbc820c87ff94092f51ca5ea
masutaniR <popohu87@gmail.com>
2021-04-10 14:43:03 +0900

    [Start] tutorial-4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_titile = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_titile.empty?
+      base_title
+    else
+      page_titile + " | " + base_title
+    end
+  end
 end
