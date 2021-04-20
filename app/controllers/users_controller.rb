649472f05123b1ce38c1915c0a917ac797b2044e
masutaniR <popohu87@gmail.com>
2021-04-20 15:54:48 +0900

    [Update] user new, create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "welcome to the Smaple App!"
+      redirect_to @user
     else
       render 'new'
     end
