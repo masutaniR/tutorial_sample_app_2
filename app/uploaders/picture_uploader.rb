8e94bf59613e8ff114d4e2c5d9b42c480bee4b45
masutaniR <popohu87@gmail.com>
2021-05-02 00:42:21 +0900

    [Finish] tutorial-13

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,12 +1,16 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
+  process resize_to_limit: [400, 400]
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -35,7 +39,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
+  def extension_allowlist
     %w(jpg jpeg gif png)
   end
 
